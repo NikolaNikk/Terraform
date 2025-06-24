9a022b2dae56df2bb9d4401b6d4bd07cbab90206
NikolaNikk <153953278+NikolaNikk@users.noreply.github.com>
2025-06-24 12:27:43 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,9 +64,4 @@ resource "azurerm_linux_virtual_machine" "vm" {
     sku       = "20.04-LTS"
     version   = "latest"
   }
-
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = file(var.ssh_public_key_path)
-  }
 }
