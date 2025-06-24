ce737cf036dbe0dd89513ef08ac235238aa4de39
NikolaNikk <153953278+NikolaNikk@users.noreply.github.com>
2025-06-24 12:23:04 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,8 +7,3 @@ variable "admin_username" {
   type        = string
   description = "Admin username for the VM"
 }
-
-variable "ssh_public_key_path" {
-  type        = string
-  description = "Path to the SSH public key"
-}
