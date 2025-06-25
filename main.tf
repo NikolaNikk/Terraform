0e32da1de88d2b94140a10bd0d822dfc0de4a247
NikolaNikk <153953278+NikolaNikk@users.noreply.github.com>
2025-06-25 10:43:14 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,8 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "myVnet"
+  or_each = toset(var.vm_names)
+  name     = "${each.key}-nic"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
